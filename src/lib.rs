bbcc6bfa6ca3816b3ba47600de7d10ac5f9336da
Samedh Gupta <gsamedh@gmail.com>
2020-12-04 12:56:09 -0500

    Day2 Solution

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,8 @@ use std::io::prelude::*;
 use std::num::ParseIntError;
 use anyhow::Result;
 
+#[macro_use]
+extern crate lazy_static;
 
 pub fn read_nums(filename: &str) -> Result<Vec<isize>> {
     let mut file = File::open(filename)?;
@@ -13,4 +15,13 @@ pub fn read_nums(filename: &str) -> Result<Vec<isize>> {
     Ok(x?) 
 }
 
+use std::io::{Lines, BufReader};
+
+pub fn read_lines(filename: &str) -> Result<Lines<BufReader<File>>> {
+    let file = File::open(filename)?;
+    let f = BufReader::new(file);
+    Ok(f.lines())
+}
+
 pub mod day1;
+pub mod day2;
